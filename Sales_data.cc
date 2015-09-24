1e985a76d64277b462e72291c26c8256358acd99
muupu <once645@gmail.com>
2015-09-24 23:39:23 +0800

    add Sales_data

---
+++ Sales_data.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+double Sales_data::avg_price() const
+{
+	if (units_sold)
+		return revenue/units_sold;
+	else
+		return 0;
+}
+
+Sales_data& Sales_data::combine(const Sales_data &rhs)
+{
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+
+istream &read(istream &is, Sales_data &item)
+{
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
+
+ostream &print(ostream &os, const Sales_data &item)
+{
+	os << item.isbn() << " " << item.units_sold << " "
+	   << item.revenue << " " << item.avg_price();
+	return os;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
+{
+	Sales_data sum = lhs;
+	sum.combine(rhs);
+	return sum;
+}
+
+int main()
+{
+	Sales_data total;
+	if (read(cin, total))
+	{
+		Sales_data trans;
+		while (read(cin, trans))
+		{
+			if (total.isbn() == trans.isbn())
+			{
+				total.combine(trans);
+			}
+			else 
+			{
+				print(cout, total) << endl;
+				toal = trans;
+			}
+		}
+		print(cout, total) << endl;
+	}
+	else
+	{
+		cerr << "No data?!" << endl;
+	}
+}
---
+++ Sales_data.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct Sales_data
+{
+	std::string isbn() const {return bookNo;}
+	Sales_data& combine(const Sales_data&);
+	double avg_price() const;
+
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+	/* data */
+};
+
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std:ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data&);
