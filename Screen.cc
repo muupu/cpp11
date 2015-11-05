78f4b2b2f55a76cfccd4d89674ca3b9ea9de5c81
muupu <once645@gmail.com>
2015-11-06 00:37:04 +0800

    Screen's member functions

--- Screen.cc
+++ Screen.cc
@@ -2,6 +2,13 @@
 class Screen {
 public:
 	using pos = std::string::size_type;
+	Screen() = default; // 因为Screen有另外一个构造函数，所以需要该构造函数
+	Screen(pos ht, pos wd, char c): height(ht), width(wd),
+	       contents(ht* wd, c) {}
+	char get() const { return contents[cursor];} // 读取光标处的字符；隐式内联
+	inline char get{pos ht, pos wd} const;       // 显示内联
+	Screen &move(pos r, pos c);                  // 能在之后被设为内联
+	
 private:
 	pos cursor = 0;
 	pos height = 0, width = 0;
