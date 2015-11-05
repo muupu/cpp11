92d215c413773b486b9bc74cdf7e76b889fe6e71
muupu <once645@gmail.com>
2015-11-06 00:42:01 +0800

    making members inline

--- Screen.cc
+++ Screen.cc
@@ -1,3 +1,4 @@
+#include <string>
 
 class Screen {
 public:
@@ -8,9 +9,22 @@ public:
 	char get() const { return contents[cursor];} // 读取光标处的字符；隐式内联
 	inline char get{pos ht, pos wd} const;       // 显示内联
 	Screen &move(pos r, pos c);                  // 能在之后被设为内联
-	
+
 private:
 	pos cursor = 0;
 	pos height = 0, width = 0;
-	std::string contents;
+    std::string contents;
 };
+
+inline Screen &Screen::move(pos r, pos c)
+{
+	pos row = r * width;
+	cursor = row + c;
+	return *this;          // 以左值的形式返回对象
+}
+
+char Screen::get(pos r, pos c) const
+{
+	pos row = r * width;
+	return contents[row + c];
+}
