8c3bab1735a3dc552d096eb02b571f3c4e95f645
muupu <once645@gmail.com>
2015-11-06 22:27:31 +0800

    7.4 class scope and members defined outside the class

--- Window_mgr.cc
+++ Window_mgr.cc
@@ -8,6 +8,8 @@ public:
 	// 将指定的Screen重置为空白
 	void clear(ScreenIndex);
 
+	ScreenIndex addScreen(const Screen&); // 添加一个Screen，返回它的编号
+
 private:
 	std::vector<Screen> screens{Screen(24,80, ' ')}; // Window_mgr开始时总是拥有一个默认初始化的Screen
 };
@@ -19,3 +21,10 @@ void Window_mgr::clear(ScreenIndex i)
 	s.contents = string(s.height * s.width, ' ');  
 }
 
+// 返回类型出现在类名之前，属于Window_mgr类的作用域之外。必须明确指定哪个类定义了它。 
+Window_mgr::ScreenIndex    
+Window_mgr::addScreen(const Screen &s)
+{
+	screens.push_back(s);
+	return screens.size() - 1;
+}
