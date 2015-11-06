6bf5292b14ea52ead858f1e36215f2720d4eb04d
muupu <once645@gmail.com>
2015-11-06 22:15:08 +0800

    the clear member of Window_mgr class

--- Window_mgr.cc
+++ Window_mgr.cc
@@ -2,6 +2,20 @@
 #include <Screen.h>
 
 class Window_mgr {
+public:
+	// 窗口中每个屏幕的编号
+	using ScreenIndex = std::vector<Screen>::size_type; 
+	// 将指定的Screen重置为空白
+	void clear(ScreenIndex);
+
 private:
 	std::vector<Screen> screens{Screen(24,80, ' ')}; // Window_mgr开始时总是拥有一个默认初始化的Screen
+};
+
+void Window_mgr::clear(ScreenIndex i)
+{
+	Screen &s = screens[i];  // s是一个引用
+	// 这里s能够访问私有成员contents（因为Window_mgr是Screen的友元类）
+	s.contents = string(s.height * s.width, ' ');  
 }
+
