9419033dac50137c6a2feee291cd5751dc08714e
muupu <once645@gmail.com>
2015-11-07 23:05:59 +0800

    defining static members

--- Account.cc
+++ Account.cc
@@ -11,7 +11,18 @@ public:
 private:
 	std::string owner;
 	double amount;
-	
+
 	static double interestRate;
 	static double initRate();
+};
+
+// 当在类的外部定义静态成员，不能重复static关键字
+void Account::rate(double newRate)
+{
+	interestRate = newRate;
 }
+
+// 必须在类的外部定义和初始化每个静态成员
+// 只能定义一次
+// 类似全局变量，一旦被定义，就将一直存在于程序的整个生命周期中
+double Account::interestRate = initRate();
