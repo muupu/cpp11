2b4d6a3ce01d4ce32faa1270fff3c41c017aefce
muupu <once645@gmail.com>
2015-11-07 22:54:57 +0800

    7.6 static class members

---
+++ Account.cc
@@ -0,0 +1,14 @@
+
+
+class Account {
+public:
+	void calculate() { amount += amount * interestRate; }
+	static double rate() { return interestRate; }
+	static void rate(double);
+
+private:
+	std::string owner;
+	double amount;
+	static double interestRate;
+	static double initRate();
+}
