7eb8666627724c447eac4a0549844d6566d5b4bc
muupu <once645@gmail.com>
2015-11-07 22:58:12 +0800

    static member functions can not be declared as const

--- Account.cc
+++ Account.cc
@@ -3,12 +3,15 @@
 class Account {
 public:
 	void calculate() { amount += amount * interestRate; }
+
+	// 静态成员函数不能声明为const。因为不包含this指针
 	static double rate() { return interestRate; }
 	static void rate(double);
 
 private:
 	std::string owner;
 	double amount;
+	
 	static double interestRate;
 	static double initRate();
 }
