fac98e097899e4f450d820ff8ff0dbed3a46c8c6
muupu <once645@gmail.com>
2015-11-08 23:09:49 +0800

    reverse iterators and const version iterators

--- SeqContainers.cc
+++ SeqContainers.cc
@@ -5,9 +5,9 @@ int main()
 {
 	list<string> a = {"Milton", "Shakespeare", "Austen"};
 	auto it1 = a.begin(); 
-	auto it2 = a.rbegin(); 
-	auto it3 = a.cbegin();
-	auto it4 = a.crbegin();
+	auto it2 = a.rbegin();  // 返回反向迭代器
+	auto it3 = a.cbegin();  // 返回const迭代器
+	auto it4 = a.crbegin(); 
 
 	return 0;
 }
