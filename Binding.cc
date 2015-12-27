ce8d74679c1e7f63efa86f4dfb11c20ecdf42510
muupu <once645@gmail.com>
2015-12-27 23:36:54 +0800

    binding function

---
+++ Binding.cc
@@ -0,0 +1,20 @@
+#include <iostream>     // std::cout
+#include <functional>   // std::bind
+
+// a function: (also works with function object: std::divides<double> my_divide;)
+double my_divide (double x, double y) {return x/y;}
+
+struct MyPair {
+  double a,b;
+  double multiply() {return a*b;}
+};
+
+int main () {
+  using namespace std::placeholders;    // adds visibility of _1, _2, _3,...
+
+  // binding functions:
+  auto fn_five = std::bind (my_divide, 10, 2);               // returns 10/2
+  std::cout << fn_five() << '\n';                           // 5
+
+  return 0;
+}
