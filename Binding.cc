3d3b3852de80440fb3df1dfb683a4148e3d6af48
muupu <once645@gmail.com>
2015-12-29 23:33:54 +0800

    binding members

--- Binding.cc
+++ Binding.cc
@@ -20,8 +20,17 @@ int main () {
   auto fn_half = std::bind(my_divide, _1, 2);       // returns x/2
   std::cout << fn_half(10) << '\n';                  // 5
 
-  auto fn_invert = std::bind (my_divide,_2,_1);            // returns y/x
+  auto fn_invert = std::bind(my_divide,_2,_1);            // returns y/x
   std::cout << fn_invert(10,2) << '\n';                    // 2/10 = 0.2
+
+  auto fn_rounding = std::bind<int> (my_divide,_1,_2);     // returns int(x/y)
+  std::cout << fn_rounding(10,3) << '\n';                  // 3
+
+  MyPair ten_two {10,2};
+  // binding members:
+  auto bound_member_fn = std::bind (&MyPair::multiply,_1); // returns x.multiply()
+  std::cout << bound_member_fn(ten_two) << '\n';           // 20
+
 
   return 0;
 }
