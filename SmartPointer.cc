c47102ff0d36f08fd482eeb91b1210d0d2a740ef
muupu <once645@gmail.com>
2015-12-30 23:52:45 +0800

    shared_ptr_copy_assign_example

--- SmartPointer.cc
+++ SmartPointer.cc
@@ -33,9 +33,25 @@ void make_shared_example()
 	std::cout << "*baz: " << baz->first << ' ' << baz->second << '\n'; // *baz: 30 40
 }
 
+void shared_ptr_copy_assign_example()
+{
+	auto p = std::make_shared<int>(42); // p所指向的对象(make_shared<int>(42))只有一个引用者(p)
+	std::cout << "p.use_count(): " << p.use_count() << '\n';
+	auto q(p); // 现在(make_shared<int>(42))有两个引用者(p，q)
+	std::cout << "p.use_count(): " << p.use_count() << '\n';
+	std::cout << "q.use_count(): " << q.use_count() << '\n';
+
+	auto r = make_shared<int>(10);
+	std::cout << "r.use_count(): " << r.use_count() << '\n';
+	r = q;
+	std::cout << "r.use_count(): " << r.use_count() << '\n';
+	std::cout << "q.use_count(): " << q.use_count() << '\n';
+}
+
 int main()
 {
 	shared_ptr_constructor_example();
 	make_shared_example();
+	shared_ptr_copy_assign_example();
 	return 0;
 }
