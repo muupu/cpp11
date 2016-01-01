e6989aca860c5c8c5892120553e1c36084152360
muupu <once645@gmail.com>
2016-01-01 22:37:22 +0800

    update

---
+++ Exercise_12/Exercise_12_7.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+shared_ptr<vector<int>> new_shared_ptr()
+{
+	// shared_ptr<vector<int>> v; // 没有分配内存和初始化，后面push_back会出错
+	auto v = make_shared<vector<int>>();
+	return v;
+}
+
+void input_vector(shared_ptr<vector<int>> v)
+{
+	int value;
+	while (cin >> value)
+		v->push_back(value);
+}
+
+void print_vector(shared_ptr<vector<int>> v)
+{
+	for (const auto &value : *v)
+		cout << value << " ";
+	cout << endl;
+}
+
+int main()
+{
+	auto v = new_shared_ptr();
+	input_vector(v);
+	print_vector(v);
+
+	return 0;
+}
