a145fd7067474be40e3e1142f57b714aa2fd9b0a
muupu <once645@gmail.com>
2016-01-03 22:45:50 +0800

    output comments

--- Unique_ptr.cc
+++ Unique_ptr.cc
@@ -5,24 +5,25 @@
 void unique_ptr_constructor_example()
 {
 	std::default_delete<int> d;
-	std::unique_ptr<int> u1;
-	std::unique_ptr<int> u2 (nullptr);
-	std::unique_ptr<int> u3 (new int);
-	std::unique_ptr<int> u4 (new int, d);
-	std::unique_ptr<int> u5 (new int, std::default_delete<int>());
-	std::unique_ptr<int> u6 (std::move(u5));
-	std::unique_ptr<int> u7 (std::move(u6));
-	std::unique_ptr<int> u8 (std::auto_ptr<int>(new int));
+	std::unique_ptr<int> u1;  // u1: null
+	std::unique_ptr<int> u2 (nullptr); // u2: null
+	std::unique_ptr<int> u3 (new int); // u3: not null
+	std::unique_ptr<int> u4 (new int, d); // u4: not null
+	std::unique_ptr<int> u5 (new int, std::default_delete<int>()); // u5: null...why?
+	std::unique_ptr<int> u6 (std::move(u5)); // u6: null
+	std::unique_ptr<int> u7 (std::move(u6)); // u7: not null
+	std::unique_ptr<int> u8 (std::auto_ptr<int>(new int)); // u8: not null
 
-	std::cout << "u1: " << (u1?"not null":"null") << '\n';
-	std::cout << "u2: " << (u2?"not null":"null") << '\n';
-	std::cout << "u3: " << (u3?"not null":"null") << '\n';
-	std::cout << "u4: " << (u4?"not null":"null") << '\n';
+	std::cout << "u1: " << (u1?"not null":"null") << '\n'; 
+	std::cout << "u2: " << (u2?"not null":"null") << '\n'; 
+	std::cout << "u3: " << (u3?"not null":"null") << '\n'; 
+	std::cout << "u4: " << (u4?"not null":"null") << '\n'; 
 	std::cout << "u5: " << (u5?"not null":"null") << '\n';
 	std::cout << "u6: " << (u6?"not null":"null") << '\n';
 	std::cout << "u7: " << (u7?"not null":"null") << '\n';
 	std::cout << "u8: " << (u8?"not null":"null") << '\n';
 }
+
 
 int main () 
 {
