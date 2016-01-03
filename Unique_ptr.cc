7537bab3f69c9e602a6e0091f810d0f8ec1add8b
muupu <once645@gmail.com>
2016-01-03 22:47:32 +0800

    unique_ptr_no_copy_assign

--- Unique_ptr.cc
+++ Unique_ptr.cc
@@ -24,9 +24,17 @@ void unique_ptr_constructor_example()
 	std::cout << "u8: " << (u8?"not null":"null") << '\n';
 }
 
+void unique_ptr_no_copy_assign()
+{
+	unique_ptr<string>p1(new string("Stegosaurus"));
+	unique_ptr<string> p2(p1);  // error: no copy for unique_ptr
+	unique_ptr<string> p3;
+	p3 = p2;  // error: no assign for unique_ptr
+}
 
 int main () 
 {
-	unique_ptr_constructor_example();
+	//unique_ptr_constructor_example();
+	unique_ptr_no_copy_assign();
 	return 0;
 }
