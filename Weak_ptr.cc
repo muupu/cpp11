f0e28b7068eddce0fe6f423c9a4b758364faee80
muupu <once645@gmail.com>
2016-01-04 23:31:33 +0800

    weak_ptr_constructor_example2

--- Weak_ptr.cc
+++ Weak_ptr.cc
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-struct C 
+struct Foo
 {
     int* data;
 };
@@ -22,6 +22,20 @@ void weak_ptr_constructor_example()
     std::cout << "wp1.use_count: " << wp1.use_count() << '\n'; // 0
     std::cout << "wp2.use_count: " << wp2.use_count() << '\n'; // 0
     std::cout << "wp3.use_count: " << wp3.use_count() << '\n'; // 1
+}
+
+void weak_ptr_constructor_example2()
+{
+    std::weak_ptr<Foo> w_ptr;
+ 
+   {
+      auto ptr = std::make_shared<Foo>();
+      w_ptr = ptr;
+      std::cout << "w_ptr.use_count() inside scope: " << w_ptr.use_count() << '\n';
+   }
+ 
+   std::cout << "w_ptr.use_count() out of scope: " << w_ptr.use_count() << '\n';
+   std::cout << "w_ptr.expired() out of scope: " << std::boolalpha << w_ptr.expired() << '\n';
 }
 
 int main () 
