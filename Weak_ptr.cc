8c1b44765828d6a50e2c254c10eb154912851da9
muupu <once645@gmail.com>
2016-01-04 23:24:15 +0800

    weak_ptr_constructor_example

---
+++ Weak_ptr.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct C 
+{
+    int* data;
+};
+
+void weak_ptr_constructor_example()
+{
+    std::cout << "--- weak_ptr_constructor_example ---" << '\n';
+
+    std::shared_ptr<int> sp (new int);
+
+    std::weak_ptr<int> wp1;
+    std::weak_ptr<int> wp2 (wp1);
+    std::weak_ptr<int> wp3 (sp);
+
+    std::cout << "use_count:\n";
+    std::cout << "wp1: " << wp1.use_count() << '\n';
+    std::cout << "wp2: " << wp2.use_count() << '\n';
+    std::cout << "wp3: " << wp3.use_count() << '\n';
+}
+
+int main () 
+{
+  weak_ptr_constructor_example();
+  return 0;
+}
