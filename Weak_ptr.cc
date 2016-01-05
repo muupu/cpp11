fcae171a15a3a29ad3779b1f17008ea0b47d4dc2
muupu <once645@gmail.com>
2016-01-05 22:56:55 +0800

    weak_ptr_lock_example

--- Weak_ptr.cc
+++ Weak_ptr.cc
@@ -39,9 +39,35 @@ void weak_ptr_constructor_example2()
    std::cout << "w_ptr.expired() out of scope: " << std::boolalpha << w_ptr.expired() << '\n'; // true
 }
 
+void observe(std::weak_ptr<int> weak) 
+{
+    if (auto observe = weak.lock()) {
+        std::cout << "\tobserve() able to lock weak_ptr<>, value=" << *observe << "\n";
+    } else {
+        std::cout << "\tobserve() unable to lock weak_ptr<>\n";
+    }
+}
+
+
+void weak_ptr_lock_example()
+{
+    std::cout << "--- weak_ptr_lock_example ---" << '\n';
+
+    std::weak_ptr<int> weak;
+    observe(weak);
+    {
+        auto shared = std::make_shared<int>(42);
+        weak = shared;
+        std::cout << "weak_ptr<> initialized with shared_ptr.\n";
+        observe(weak);
+    }
+    observe(weak);
+}
+
 int main () 
 {
   weak_ptr_constructor_example();
   weak_ptr_constructor_example2();
+  weak_ptr_lock_example();
   return 0;
 }
