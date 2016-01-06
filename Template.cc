230ccde00114dc12489e961f6fbab847981beb80
muupu <once645@gmail.com>
2016-01-06 23:41:09 +0800

    nontype_template_parameters_example

--- Template.cc
+++ Template.cc
@@ -15,11 +15,14 @@ int compare(const T &v1, const T &v2)
 
 int template_example()
 {
+	cout << "--- template_example ---" << endl;
 	cout << compare(1, 0) << endl;
 	vector<int> vec1{1, 2, 3}, vec2{1, 2, 3}, vec3{4, 5, 6};
 	cout << compare(vec1, vec2) << endl;
 	cout << compare(vec1, vec3) << endl;
 }
+
+// 非类型模板参数
 
 template<unsigned N, unsigned M>
 int compare(const char (&p1)[N], const char (&p2)[M])
@@ -27,9 +30,18 @@ int compare(const char (&p1)[N], const char (&p2)[M])
 	return strcmp(p1, p2);
 }
 
+
+int nontype_template_parameters_example()
+{
+	cout << "--- nontype_template_parameters_example ---" << endl;
+	cout << compare("hi", "hi") << endl;
+	cout << compare("hi", "hello") << endl;
+}
+
 int main()
 {
 	template_example();
+	nontype_template_parameters_example();
 
 	return 0;
 }
