0872e9195c6b417fab28413182c0f238cae38917
muupu <once645@gmail.com>
2016-01-08 23:57:47 +0800

    Blob construction

--- Blob.h
+++ Blob.h
@@ -1,5 +1,10 @@
 #include <vector>
 
 template <typename T> class Blob{
-	
+	public:
+		typedef T value_type;
+		typedef typename std::vector<T>::size_type size_type;
+
+		Blob();
+		Blob()(std::initializer_list<T> il);
 };
