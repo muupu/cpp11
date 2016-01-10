c986dfa0c077374a83114db03dfcfdf6b0e6fd07
muupu <once645@gmail.com>
2016-01-10 21:50:55 +0800

    smart_pointers_and_dynamic_arrays

--- DynamicArrays.cc
+++ DynamicArrays.cc
@@ -1,10 +1,17 @@
 #include <iostream>
-
+#include <memory>
 using namespace std;
 
 void initializing_dynamic_arrays()
 {
 	int* p = new int[0]; // 动态分配空数组是合法的
+}
+
+void smart_pointers_and_dynamic_arrays()
+{
+	// 用unique_ptr管理动态数组，必须在对象类型后面跟一对空方括号
+	unique_ptr<int[]> up(new int[10]);
+	up.release();
 }
 
 int main()
