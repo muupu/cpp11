93ee41f5d0bad7a196a79c6a88d518d88a46dc6d
muupu <once645@gmail.com>
2016-01-10 22:13:03 +0800

    Exercise_12_23

---
+++ Exercise_12/Exercise_12_23.cc
@@ -0,0 +1,19 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+char* strcat(char* str1, char* str2)
+{
+
+}
+
+int main()
+{
+	char *str1 = "abcd";
+	char *str2 = "efgh";
+	char *newStr = new char[strlen(str1) + strlen(str2) + 1];
+	strcpy(newStr, str1);
+	strcat(newStr, str2);
+	cout << "newStr:" << newStr << endl;
+}
