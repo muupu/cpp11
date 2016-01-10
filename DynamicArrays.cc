b9a017cba670474067ab1465123b1d5de2765122
muupu <once645@gmail.com>
2016-01-10 22:07:07 +0800

    use get to obtain a built-in pointer

--- DynamicArrays.cc
+++ DynamicArrays.cc
@@ -16,6 +16,12 @@ void smart_pointers_and_dynamic_arrays()
 	// 如果希望使用shared_ptr管理一个动态数组，必须提供自己定义的删除器
 	shared_ptr<int> sp(new int[10], [](int *p) { delete[] p; });
 	sp.reset();
+
+	// shared_ptr未定义下标运算符
+	// 并且智能指针类型不支持指针算术运算
+    // 为了访问数组中的元素，必须用get获取一个内置指针，然后用它来访问数组元素
+    for (size_t i = 0; i != 10; ++i)
+    	*(sp.get() + i) = i;
 }
 
 int main()
