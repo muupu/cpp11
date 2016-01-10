cd12b9354d00dbda295611ab64ae5deeebe7ad29
muupu <once645@gmail.com>
2016-01-10 22:03:16 +0800

    if we want to use a shared_ptr to manage a dynamic array, we must provide deleter

--- DynamicArrays.cc
+++ DynamicArrays.cc
@@ -11,7 +11,11 @@ void smart_pointers_and_dynamic_arrays()
 {
 	// 用unique_ptr管理动态数组，必须在对象类型后面跟一对空方括号
 	unique_ptr<int[]> up(new int[10]);
-	up.release();
+	up.release(); // int[]说明up指向一个数组，当release时，会自动使用delete[]。
+
+	// 如果希望使用shared_ptr管理一个动态数组，必须提供自己定义的删除器
+	shared_ptr<int> sp(new int[10], [](int *p) { delete[] p; });
+	sp.reset();
 }
 
 int main()
