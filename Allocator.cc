5a48eca2f75afba3b40ed532a9ff7fa113d57441
muupu <once645@gmail.com>
2016-01-11 23:59:10 +0800

    allocators allocate unconstructed memory

--- Allocator.cc
+++ Allocator.cc
@@ -6,5 +6,10 @@ int main()
 {
 	allocator<string> alloc;
 	auto const p = alloc.allocate(n); // 分配n个未初始化的string
-	
+
+	// allocator分配未构造的内存
+	auto q = p;
+	alloc.construct(q++);
+	alloc.construct(q++, 10, 'c');
+	alloc.construct(q++, "hi");
 }
