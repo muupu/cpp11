97abf71c0d22233f64c2e9f2ad78b4bfdfed60fe
qiaoda.zqd <qiaoda.zqd@alibaba-inc.com>
2016-01-18 16:49:30 +0800

    Func_Parameter

---
+++ Func_Parameter.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;  
+
+void Print(const char* str)   
+{
+    cout<<"holle "<<str<<endl;   
+}
+
+template<typename  Fun>
+void CallBackFun(void(*Fun)(), const char* str)   
+{   
+    (*Fun)(str);   
+}   
+
+int main()   
+{   
+    CallBackFun(&Print, "world");
+    return  0;
+}   
