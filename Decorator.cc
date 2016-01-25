6fe5230a1e1ee99b1394b7801427dbd75fadf26e
muupu <once645@gmail.com>
2016-01-25 23:01:25 +0800

    add Hello

--- Decorator.cc
+++ Decorator.cc
@@ -6,3 +6,13 @@ public:
     virtual void show() = 0;
                     // 子类只有实现该接口才可进行对象的实例化工作
 };
+
+class Hello :public IHello
+{
+private:
+    std::string _name;
+public:
+    Hello(const std::string& name):_name(name){}
+    void show() { std::cout << "hello " + _name << std::endl;}
+};
+
