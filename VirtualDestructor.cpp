68ea15ea718ef47468a85df3c48cb0bfad6b5848
muupu <once645@gmail.com>
2016-01-26 09:44:50 +0800

    add VirtualDestructor

---
+++ VirtualDestructor.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+
+
+
+	//cout<<pA->testStr<<endl;
+	//cout<<pA->TestA()<<endl;
+
+//class C : public B
+//{
+//public:
+//	virtual ~C()
+//	{
+//		cout<<"~C"<<endl;
+//	}
+//};
