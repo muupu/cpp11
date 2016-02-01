a3cd13f947a2bda6c47814e68c3821f9329c7d59
muupu <once645@gmail.com>
2016-02-01 13:27:27 +0800

    test

--- AlgorithmTest.cc
+++ AlgorithmTest.cc
@@ -14,7 +14,7 @@ void test_partial_sort()
 {
 	std::array<int, 10> s{5, 7, 4, 2, 8, 6, 1, 9, 0, 3};
  
-    std::partial_sort(s.begin(), s.begin() + 3, s.end());
+    std::partial_sort(s.begin(), s.begin() + 5, s.end());
     for (int a : s) {
         std::cout << a << " ";
     } 
