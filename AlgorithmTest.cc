caecdc42e4913b32e86b50c32bcc33bbd46fe329
muupu <once645@gmail.com>
2016-02-01 10:59:31 +0800

    add AlgorithmTest.cc

---
+++ AlgorithmTest.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+ 
+
+
+/***  Sorting operations (on sorted ranges) ***/
+// std::nth_element
+void test_nth_element()
+{
+	std::vector<int> v{5, 6, 4, 3, 2, 6, 7, 9, 3};
+ 
+    std::nth_element(v.begin(), v.begin() + v.size()/2, v.end());
+    std::cout << "The median is " << v[v.size()/2] << '\n';
+ 
+    std::nth_element(v.begin(), v.begin()+1, v.end(), std::greater<int>());
+    std::cout << "The second largest element is " << v[1] << '\n';
+}
+
+int main()
+{
+	
+	/***  Sorting operations (on sorted ranges) ***/
+    test_nth_element();
+}
