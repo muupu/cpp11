598e62f3f152a858c42522df8fc063e55fcd07fb
muupu <once645@gmail.com>
2016-02-03 23:57:05 +0800

    imp: TextQuery construction

--- 12_TextQuery/TextQuery.h
+++ 12_TextQuery/TextQuery.h
@@ -26,4 +26,17 @@ private:
 TextQuery::TextQuery(ifstream &is): file(new vector<string>)
 {
 	string text;
+	while (getline(is, text))
+	{
+		file->push_back(text);
+		int n = file->size() - 1;
+		istringstream line(text);
+		string word;
+		while (line >> word) {
+			auto &lines = wm[word];
+			if (!lines)
+				lines.reset(new set<line_no>);
+			lines->insert(n);
+		}
+	}
 }
