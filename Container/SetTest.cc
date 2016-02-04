8f270d3c2007ba474e8a4ace692ea3e1daf30d0b
muupu <once645@gmail.com>
2016-02-04 14:01:23 +0800

    add: print_unordered_set

--- Container/SetTest.cc
+++ Container/SetTest.cc
@@ -4,20 +4,32 @@
 #include <array>
 #include <unordered_set>
 
+void print_unordered_set(std::unordered_set<std::string>& myset)
+{
+  std::cout << "myset contains:";
+  for (const std::string& x: myset) std::cout << " " << x;
+  std::cout <<  std::endl;
+}
+
 void test_unordered_set_insert()
 {
   std::unordered_set<std::string> myset = {"yellow","green","blue"};
   std::array<std::string,2> myarray = {"black","white"};
   std::string mystring = "red";
+  print_unordered_set(myset);
 
   myset.insert (mystring);                        // copy insertion
-  myset.insert (mystring+"dish");                 // move insertion
-  myset.insert (myarray.begin(), myarray.end());  // range insertion
-  myset.insert ( {"purple","orange"} );           // initializer list insertion
+  print_unordered_set(myset);
 
-  std::cout << "myset contains:";
-  for (const std::string& x: myset) std::cout << " " << x;
-  std::cout <<  std::endl;
+  myset.insert (mystring+"dish");                 // move insertion
+  print_unordered_set(myset);
+
+  myset.insert (myarray.begin(), myarray.end());  // range insertion
+  print_unordered_set(myset);
+
+  myset.insert ( {"purple","orange"} );           // initializer list insertion
+  print_unordered_set(myset);
+
 }
 
 int main ()
