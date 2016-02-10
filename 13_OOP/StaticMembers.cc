86c1bf3b32893a6a9abaee7acba5bfdd95709d6e
muupu <once645@gmail.com>
2016-02-10 14:05:15 +0800

    access through a Derived objec

--- 13_OOP/StaticMembers.cc
+++ 13_OOP/StaticMembers.cc
@@ -12,7 +12,7 @@ public:
 class Derived : public Base {
 public:
 	void f( const Derived&);
-	static void statemen()  // 添加了这个，Derived::statemen()会输出：Derived statemen
+	static void statemen()  // 添加了这个(不管有没有static)，Derived::statemen()都会输出：Derived statemen
 	{
 		cout << "Derived statemen" << endl;
 	}
@@ -22,6 +22,8 @@ void Derived::f(const Derived &derived_obj)
 {
 	Base::statemen(); // 使用基类访问基类的静态成员
 	Derived::statemen(); // 使用派生类访问基类的静态成员
+	derived_obj.statemen(); // 派生类对象访问基类的静态成员
+	statemen();       // this指针访问
 }
 
 int main()
