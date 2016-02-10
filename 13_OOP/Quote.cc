cdb709a8d0f692bdea5384af0f5a2b8a8ee02317
muupu <once645@gmail.com>
2016-02-10 21:40:24 +0800

    derived class constructor initialize its direct base class only

--- 13_OOP/Quote.cc
+++ 13_OOP/Quote.cc
@@ -29,19 +29,16 @@ protected:
 	double discount = 0.0;
 };
 
-class Bulk_quote : public Quote {
+class Bulk_quote : public Disc_quote {
 public:
 	Bulk_quote() = default;
 	Bulk_quote(const std::string&, double, std::size_t, double);
 	double net_price(std::size_t) const override; 
-
-private:
-	std::size_t min_qty = 0;
-	double discount = 0.0;
 };
 
-Bulk_quote::Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
-	Quote(book, p), min_qty(qty), discount(disc) 
+// 派生类构造函数只初始化它的直接基类
+Bulk_quote::Bulk_quote(const std::string& book, double price, std::size_t qty, double disc) :
+	Disc_quote(book, price, qty, disc) 
 	{ }
 
 double Bulk_quote::net_price(size_t cnt) const
