9234e36ad436544bf6e79532b769281c6b4eee1b
muupu <once645@gmail.com>
2016-02-10 13:41:15 +0800

    using members of the base class from the derived class

--- 13_OOP/Quote.cc
+++ 13_OOP/Quote.cc
@@ -32,4 +32,12 @@ Bulk_quote::Bulk_quote(const std::string& book, double p, std::size_t qty, doubl
 	Quote(book, p), min_qty(qty), discount(disc) 
 	{ }
 
+double Bulk_quote::net_price(size_t cnt) const
+{
+	if (cnt >= min_qty)
+		return cnt * (1 - discount) * price;
+	else
+		return cnt * price;
+}
+
 
