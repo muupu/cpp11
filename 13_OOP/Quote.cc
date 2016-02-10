58f33a2495db6b9296cf480c140e70de2196b007
muupu <once645@gmail.com>
2016-02-10 11:46:22 +0800

    derived class constructors

--- 13_OOP/Quote.cc
+++ 13_OOP/Quote.cc
@@ -27,3 +27,9 @@ private:
 	std::size_t min_qty = 0;
 	double discount = 0.0;
 };
+
+Bulk_quote::Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
+	Quote(book, p), min_qty(qty), discount(disc) 
+	{ }
+
+
