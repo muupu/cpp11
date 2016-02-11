dc89b7f0e67a165de45abf59f519674fcc5970e4
muupu <once645@gmail.com>
2016-02-11 20:48:50 +0800

    add:thread_lambda.cc

---
+++ Thread/thread_lambda.cc
@@ -0,0 +1,17 @@
+#include <thread>
+#include <iostream>
+#include <vector>
+int main()
+{
+    std::vector<std::thread> threads; // 使用vector来存放每个线程
+    for(int i = 0; i < 5; ++i){
+        threads.push_back(std::thread([](){
+            std::cout << "Hello from lamda thread " << std::this_thread::get_id() << std::endl;
+        }));
+    }
+    for(auto& thread : threads){
+        thread.join();
+    }
+    std::cout<<"Main Thread"<<"\t"<<std::this_thread::get_id()<<std::endl;
+    return 0;
+}
