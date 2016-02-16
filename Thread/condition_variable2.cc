45ec72ce11d0373b06e47e012840663e70ab7e62
muupu <once645@gmail.com>
2016-02-16 22:37:28 +0800

    add: condition_variable2.cc

---
+++ Thread/condition_variable2.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+class Foo
+{
+public:
+	Foo()
+		: flag_(0)
+		, thread1_(std::bind(&Foo::threadFunc1, this))
+		, thread2_(std::bind(&Foo::threadFunc2, this))
+	{
+	}
+
+	~Foo()
+	{
+		thread1_.join();
+		thread2_.join();
+	}
+
+private:
+	void threadFunc1()
+	{
+		{
+			std::unique_lock<std::mutex> ul(mutex_);
+			while (0 == flag_) {
+				cond_.wait(ul);
+			}
+			std::cout << flag_ << std::endl;
+		}
+	}
+
+	void threadFunc2()
+	{
+		// 为了测试，等待3秒
+		std::this_thread::sleep_for((std::chrono::milliseconds(3000)));
+		std::unique_lock<std::mutex> ul(mutex_);
+		flag_ = 100;
+		cond_.notify_one();
+	}
+
+	int flag_;
+	std::mutex mutex_;
+	std::condition_variable cond_;
+	std::thread thread1_;
+	std::thread thread2_;
+};
+
+int main()
+{
+	Foo f;
+
+	system("pause");
+	return 0;
+}
