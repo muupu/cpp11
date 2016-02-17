f73fa95ddc1ab2caf1e53c0a2a30a598659c32b3
muupu <once645@gmail.com>
2016-02-17 17:42:52 +0800

    comments

--- Ref_Member.cc
+++ Ref_Member.cc
@@ -1,3 +1,5 @@
+// 在C++中使用引用类型的成员变量
+
 #include <iostream>
 using namespace std;
 
@@ -6,7 +8,7 @@ class Test
 private:
     int &a;
 public:
-    Test(int &b) : a(b)
+    Test(int &b) : a(b) // 引用类型的成员变量必须在构造函数的初始化列表中进行初始化
     {
     }
     void Modify(int value)
@@ -18,8 +20,8 @@ int main()
 {
     int b = 3;
     Test test(b);
-    cout <<"b="<<b<<endl;
+    cout <<"b="<<b<<endl;  // b=3
     test.Modify(4);
-    cout <<"b="<<b<<endl;
+    cout <<"b="<<b<<endl;  // b=4
     return 0;
 }
